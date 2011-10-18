fa116112835487882c856c230738ea956480a6a5
Xiaochun Shen <snowdonshen@gmail.com>
2011-10-18 10:37:44 +0800

    Initial Commit

---
+++ Homepwner/HomepwnerAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  HomepwnerAppDelegate.h
+//  Homepwner
+//
+//  Created by  Chuns on 11-10-18.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomepwnerAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
